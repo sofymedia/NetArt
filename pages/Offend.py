4475f0d226d813e5c3182dc2b207b6b9ffc514e3
sofymedia <sofya.mashukova@gmail.com>
2025-11-22 22:55:13 -0500

    Add files via upload

---
+++ pages/Offend.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.image("RoaryOffendWIP.png")
+st.write(f"\"Why did you even come here {st.session_state.name}!?"
+         f" Someone should reread the Student Code of Conduct!\"")
