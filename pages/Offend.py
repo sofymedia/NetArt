914486b9187fcd477b08e772fab54137c1d2092a
sofymedia <sofya.mashukova@gmail.com>
2025-12-07 16:49:53 -0500

    Delete pages directory

--- pages/Offend.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-st.image("RoaryOffendWIP.png")
-st.write(f"\"Why did you even come here {st.session_state.name}!?"
-         f" Someone should reread the Student Code of Conduct!\"")
